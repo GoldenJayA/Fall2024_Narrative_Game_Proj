cc3eec57df70d81e7b23953ab767762b2b715602
GoldenJayA <100546408+GoldenJayA@users.noreply.github.com>
2024-11-23 21:16:32 -0500

    Revert "Dialogue & Tethers Talk to Each other" (#7)

--- Assets/Scripts/Characters/NPC.cs
+++ Assets/Scripts/Characters/NPC.cs
@@ -1,22 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using Ink.Runtime;
-using System;
 using UnityEngine;
 
 public class NPC : Character
 {
-    [SerializeField]
-    private TextAsset inkJSONAsset = null;
-    [SerializeField]
-    private Story story;
-
-
-    public event Action<Story> OnSelectStory;
     // Start is called before the first frame update
     void Start()
     {
-        story = new Story(inkJSONAsset.text);
+        
     }
 
     // Update is called once per frame
@@ -48,7 +39,7 @@ public class NPC : Character
     {
         if(other.CompareTag("Player"))
         {
-            other.GetComponent<Player>().TalkToNPC(gameObject, story);
+            other.GetComponent<Player>().TalkToNPC(transform);
             
         }
     }
