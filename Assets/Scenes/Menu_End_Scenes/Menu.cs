0192f229ed0929fc6c222db391374fe1f2d7b63d
SenseiCanada <117216802+SenseiCanada@users.noreply.github.com>
2024-12-02 16:35:20 -0500

    Revert "edited end scenes and added credits"
    This reverts commit cd507191f26c4a4b0036674d3ea5f3c2d37157fa.

--- Assets/Scenes/Menu_End_Scenes/Menu.cs
+++ Assets/Scenes/Menu_End_Scenes/Menu.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject creditsObject;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,16 +13,6 @@ public class Menu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void ShowCredits()
-    {
-        creditsObject.SetActive(true);
-    }
-
-    public void HideCredits()
-    {
-        creditsObject.SetActive(false);
     }
 
 }
